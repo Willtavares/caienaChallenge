ba21bc3c4fa6916ef9d2b1d1a57fe458807ef835
Willtavares <willtavares@outlook.com>
2023-06-07 04:59:30 -0300

    Refactor and test Wip

--- src/app/components/search-users/search-users.component.spec.ts
+++ src/app/components/search-users/search-users.component.spec.ts
@@ -1,15 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of, throwError } from 'rxjs';
+import { UserInfo } from './../../interfaces/user-info';
+import { SearchUsersService } from './../../services/search-users.service';
 import { SearchUsersComponent } from './search-users.component';
 
 describe('SearchUsersComponent', () => {
   let component: SearchUsersComponent;
   let fixture: ComponentFixture<SearchUsersComponent>;
+  let searchUsersServiceSpy: jasmine.SpyObj<SearchUsersService>;
 
   beforeEach(() => {
+    // Cria um objeto spy para o serviço SearchUsersService
+    searchUsersServiceSpy = jasmine.createSpyObj('SearchUsersService', [
+      'findUser',
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [SearchUsersComponent]
+      declarations: [SearchUsersComponent],
+      providers: [
+        { provide: SearchUsersService, useValue: searchUsersServiceSpy },
+      ],
     });
+
     fixture = TestBed.createComponent(SearchUsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +29,55 @@ describe('SearchUsersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call findUser service method when onSearch is called', () => {
+    // Simula um nome de usuário para pesquisar
+    const username = 'testuser';
+
+    // Aciona o método onSearch
+    component.name = username;
+    component.onSearch();
+
+    // Verifica se o método findUser do serviço foi chamado com o nome de usuário correto
+    expect(searchUsersServiceSpy.findUser).toHaveBeenCalledWith(username);
+  });
+
+  it('should set user and status on successful user retrieval', () => {
+    // Cria um objeto UserInfo simulado
+    const user: UserInfo = {
+      avatar_url: 'https://example.com/avatar.jpg',
+      html_url: 'https://example.com/user',
+      followers: undefined,
+      following: undefined,
+      public_repos: undefined,
+    };
+
+    // Configura o serviço para retornar o usuário simulado
+    searchUsersServiceSpy.findUser.and.returnValue(of(user));
+
+    // Aciona o método onSearch
+    component.name = 'testuser';
+    component.onSearch();
+
+    // Verifica se o usuário e o status foram definidos corretamente
+    expect(component.user).toEqual(user);
+    expect(component.status).toBe(200);
+  });
+
+  it('should set status on error during user retrieval', () => {
+    // Simula um erro no serviço
+    const errorMessage = 'User not found';
+    const errorStatus = 404;
+    searchUsersServiceSpy.findUser.and.returnValue(
+      throwError({ status: errorStatus, message: errorMessage })
+    );
+
+    // Aciona o método onSearch
+    component.name = 'testuser';
+    component.onSearch();
+
+    // Verifica se o status foi definido corretamente
+    expect(component.status).toBe(errorStatus);
   });
 });
--- src/app/components/search-users/search-users.component.ts
+++ src/app/components/search-users/search-users.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { UserInfo } from './../../interfaces/user-info';
 import { SearchUsersService } from './../../services/search-users.service';
@@ -9,43 +9,45 @@ import { SearchUsersService } from './../../services/search-users.service';
   templateUrl: './search-users.component.html',
   styleUrls: ['./search-users.component.scss'],
 })
-export class SearchUsersComponent {
-  name: string = '';
-  user!: UserInfo | null;
+export class SearchUsersComponent implements OnDestroy {
+  name = '';
+  user: UserInfo | null = null;
   findUser = new Subject<string>();
-  status: number = 0;
+  status: number | null = 0;
+  private subscription = new Subscription();
 
-  /**
-   *
-   */
   constructor(private usersService: SearchUsersService) {
-    this.findUser
-      .pipe(debounceTime(700), distinctUntilChanged())
-      .subscribe((value) => {
-        if (value) {
-          this.usersService
-            .findUser(this.name)
-            .pipe(debounceTime(700), distinctUntilChanged())
-            .subscribe(
-              (res) => {
-                this.status = 200;
-                this.user = res;
-                console.log(8787, this.user);
-              },
-              (error) => {
-                // Tratamento do erro aqui
-                console.error('Ocorreu um erro na busca do usuário:');
-                this.status = error.status;
-              }
+    this.subscription.add(
+      this.findUser
+        .pipe(debounceTime(700), distinctUntilChanged())
+        .subscribe((value) => {
+          if (value) {
+            this.subscription.add(
+              this.usersService.findUser(value).subscribe(
+                (res) => {
+                  this.status = 200;
+                  this.user = res;
+                  console.log(8787, this.user);
+                },
+                (error) => {
+                  console.error('Ocorreu um erro na busca do usuário:');
+                  this.status = error.status;
+                }
+              )
             );
-        } else {
-          this.user = null;
-          this.status = 0;
-        }
-      });
+          } else {
+            this.user = null;
+            this.status = 0;
+          }
+        })
+    );
   }
 
   onSearch() {
     this.findUser.next(this.name);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
